42d4c3bc192266257fd09e04e9d043fbef643289
Ragahleak Bartee <rbartee@wlapcs.org>
2017-04-11 17:41:42 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(600,400);
+    
+}
+
+function draw(){
+    background(0);
+    
+    fill(255,255,255)
+    ellipse(150,150,100,100);
+    
+    fill(183,18,18)
+    rect(300,100,100,100);
+}
+
+
+
+
+
